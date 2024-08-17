c1a986f32977261446493e8989a961e0e7c32d38
yilmazsenem <senem.yilmazz@gmail.com>
2024-08-18 01:38:12 +0300

    Chapter 1 - Q04 initialize.

---
+++ src/odevler/bolum01/Q04.java
@@ -0,0 +1,20 @@
+package odevler.bolum01;
+
+public class Q04 {
+    public static void main(String[] args) {
+        int sayi1 = 1;
+        int sayi2 = 2;
+        int sayi3 = 3;
+        int sayi4 = 4;
+        int sayi5 = (int) Math.pow(sayi1,sayi2);
+        int sayi6 = (int) Math.pow(sayi2,sayi2);
+        int sayi7 = (int) Math.pow(sayi3,sayi2);
+        int sayi8 = (int) Math.pow(sayi4,sayi2);
+        int sayi9 = (int) Math.pow(sayi1,sayi3);
+        int sayi10 = (int) Math.pow(sayi2,sayi3);
+        int sayi11 = (int) Math.pow(sayi3,sayi3);
+        int sayi12 = (int) Math.pow(sayi4,sayi3);
+        System.out.println (sayi1);
+
+    }
+}
