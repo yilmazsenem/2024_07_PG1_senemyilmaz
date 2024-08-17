8e7d8fee96b8dc2a8517ff06d0c7c5911c32a8cc
yilmazsenem <senem.yilmazz@gmail.com>
2024-08-17 23:04:17 +0300

    Chapter 1 - Q03 is done.

--- src/odevler/bolum01/Q03.java
+++ src/odevler/bolum01/Q03.java
@@ -2,8 +2,8 @@ package odevler.bolum01;
 
 public class Q03 {
     public static void main(String[] args) {
-        System.out.println("    J     A     V    V     A");
-        System.out.println("    J    A A     V  V     A A");
+        System.out.println("    J     A     V     V    A");
+        System.out.println("    J    A A     V   V    A A");
         System.out.println("J   J   AAAAA     V V    AAAAA");
         System.out.println(" J J   A     A     V    A     A");
     }
