642f8a229ee117cf996d1520541c00e4a870d550
yilmazsenem <senem.yilmazz@gmail.com>
2024-08-17 20:04:14 +0300

    Chapter 1 - Q02 done

--- src/Merhaba.java
+++ src/Merhaba.java
@@ -1,5 +1,5 @@
-class Merhaba {
+public class Merhaba {
 	public static void main(String[] args) {
-		System.out.println("Merhaba 2024_07_P1");
+	System.out.println("Merhaba 2024_07_PG1");	
 	}
 }
--- src/odevler/bolum01/Q02.java
+++ src/odevler/bolum01/Q02.java
@@ -1,4 +1,11 @@
 package odevler.bolum01;
 
 public class Q02 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+    }
 }
