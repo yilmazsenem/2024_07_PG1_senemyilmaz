a404dee05ac95a57eccef9c5ff0ec11ed3ae3245
yilmazsenem <senem.yilmazz@gmail.com>
2024-08-23 16:33:06 +0300

    Chapter 1 - Q04 is done.

--- src/odevler/bolum01/Q04.java
+++ src/odevler/bolum01/Q04.java
@@ -2,23 +2,27 @@ package odevler.bolum01;
 
 public class Q04 {
     public static void main(String[] args) {
-        int sayi1 = 1;
-        int sayi2 = 2;
-        int sayi3 = 3;
-        int sayi4 = 4;
-        int sayi5 = (int) Math.pow(sayi1,sayi2);
-        int sayi6 = (int) Math.pow(sayi2,sayi2);
-        int sayi7 = (int) Math.pow(sayi3,sayi2);
-        int sayi8 = (int) Math.pow(sayi4,sayi2);
-        int sayi9 = (int) Math.pow(sayi1,sayi3);
-        int sayi10 = (int) Math.pow(sayi2,sayi3);
-        int sayi11 = (int) Math.pow(sayi3,sayi3);
-        int sayi12 = (int) Math.pow(sayi4,sayi3);
-        int[] list1 = {sayi1, sayi5, sayi9};
-        int[] list2 = {sayi2, sayi6, sayi10};
-        int[] list3 = {sayi3, sayi7, sayi11};
-        int[] list4 = {sayi4, sayi8, sayi12};
-        System.out.println (list2);
+        int[][] table = new int[4][3];
+        table [0][0] = 1;
+        table [1][0] = 2;
+        table [2][0] = 3;
+        table [3][0] = 4;
 
-    }
-}
+        table[0][1] = (int) Math.pow(table[0][0],table[1][0]);
+        table[0][2] = (int) Math.pow(table[0][0],table[2][0]);
+
+        table[1][1] = (int) Math.pow(table[1][0],table[1][0]);
+        table[1][2] = (int) Math.pow(table[1][0],table[2][0]);
+
+        table[2][1] = (int) Math.pow(table[2][0],table[1][0]);
+        table[2][2] = (int) Math.pow(table[2][0],table[2][0]);
+
+        table[3][1] = (int) Math.pow(table[3][0],table[1][0]);
+        table[3][2] = (int) Math.pow(table[3][0],table[2][0]);
+
+        System.out.println(table[0][0] + " " + table[0][1] + " " + table[0][2]);
+        System.out.println(table[1][0] + " " + table[1][1] + " " + table[1][2]);
+        System.out.println(table[2][0] + " " + table[2][1] + " " + table[2][2]);
+        System.out.println(table[3][0] + " " + table[3][1] + " " + table[3][2]);
+    }}
+
