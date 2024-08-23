ed75ac4163962336639a35b5013e63c0699c73f7
yilmazsenem <senem.yilmazz@gmail.com>
2024-08-23 22:11:57 +0300

    Chapter 1 - Q09 is done.

---
+++ src/odevler/bolum01/Q09.java
@@ -0,0 +1,11 @@
+package odevler.bolum01;
+
+public class Q09 {
+    public static void main(String[] args) {
+        double width = 4.5;
+        double height = 5.5;
+        double rectanglearea = (width*height);
+        System.out.println (rectanglearea);
+
+    }
+}
