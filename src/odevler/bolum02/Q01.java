2aa60d3258f35917c0375f6c4225a27d3c800226
yilmazsenem <senem.yilmazz@gmail.com>
2024-08-24 00:18:36 +0300

    Chapter 1 - Q13 is initalizing.

--- src/odevler/bolum02/Q01.java
+++ src/odevler/bolum02/Q01.java
@@ -2,6 +2,7 @@ package odevler.bolum02;
 
 public class Q01 {
     public static void main(String[] args) {
-
+        double fahrenheit = ((9.0 / 5) * (celsius + 32));
+        System.out.println ((52 Celsius)
     }
 }
