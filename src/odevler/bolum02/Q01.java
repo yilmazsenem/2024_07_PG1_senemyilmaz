6777cd53f64bd19e068dc999c1f80ca4e7439447
yilmazsenem <senem.yilmazz@gmail.com>
2024-08-24 00:25:58 +0300

    Chapter 1 - Q13 is completed.

--- src/odevler/bolum02/Q01.java
+++ src/odevler/bolum02/Q01.java
@@ -2,7 +2,10 @@ package odevler.bolum02;
 
 public class Q01 {
     public static void main(String[] args) {
+        int celsius = 52;
         double fahrenheit = ((9.0 / 5) * (celsius + 32));
-        System.out.println ((52 Celsius)
+        System.out.println (fahrenheit);
+        System.out.println ((celsius) + " " + "celsius is" + " " + (fahrenheit) + " " + "fahrenheit" + ".");
+
     }
 }
