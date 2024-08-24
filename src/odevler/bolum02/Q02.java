14d473d670f1759a063d5a9f1a284d3c0a478ad9
yilmazsenem <senem.yilmazz@gmail.com>
2024-08-24 14:31:36 +0300

    Chapter 1 - Q02 is completed.

--- src/odevler/bolum02/Q02.java
+++ src/odevler/bolum02/Q02.java
@@ -1,8 +1,17 @@
 package odevler.bolum02;
 
+import java.util.Scanner;
 public class Q02 {
     public static void main(String[] args) {
-        int
-        double area =
+        Scanner scanner = new Scanner(System.in);
+        System.out.print ("Enter the radius and length of a cylinder:   ");
+        double radius = scanner.nextDouble();
+        double length = scanner.nextDouble();
+        double pi = Math.PI;
+        double area = radius*radius*pi;
+        double volume = area*length;
+        System.out.println("Area of cylinder is" + " " + area + " " + ".");
+        System.out.println("Volume of cylinder is" + " " + volume + " " + ".");
+
     }
 }
